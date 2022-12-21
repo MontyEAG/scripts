ba820a31299b5ad746a30486e3d36bb34a4d3bf1
MontyEAG <112272316+MontyEAG@users.noreply.github.com>
2022-12-20 21:24:26 -0600

    Update montysmenu1.lua

--- montysmenu1.lua
+++ montysmenu1.lua
@@ -10,9 +10,6 @@ SectionWalk:NewSlider("Walk Speed", "Tylers so gay lol", 500, 16, function(s) --
     end)
 Player.Character.Humanoid.WalkSpeed = getgenv().WalkSpeedValue;
 end)
-SectionWalk:NewButton("FAST", "Sets Walk Speed to 120", function()
-    s = 120
-end)
 local SectionAfk = Main:NewSection("ANTI AFK")
 SectionAfk:NewButton("LAUNCH AFK SCRIPT", ".", function()
     print("Clicked")
