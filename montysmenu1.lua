38eb3654eadc12a8a10c111885201a72389d8181
MontyEAG <112272316+MontyEAG@users.noreply.github.com>
2022-12-24 03:48:18 -0600

    Update montysmenu1.lua

--- montysmenu1.lua
+++ montysmenu1.lua
@@ -1,1 +1,30 @@
-game.Players.LocalPlayer:Kick("NO MORE WARNINGS. YOU ARE GETTING SWATTED. POLICE WILL BE CALLED TO YOU ADDRESS WITHIN 5 HOURS. YOU HAVE BEEN WARNED.")
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Monty's Menu", "GrapeTheme")
+local Main = Window:NewTab("General")
+local SectionWalk = Main:NewSection("Walk Speed")
+SectionWalk:NewSlider("Walk Speed", "Tylers so gay lol", 500, 16, function(s) -- 500 (MaxValue) | 16 (MinValue)
+    getgenv().WalkSpeedValue = s; 
+    local Player = game:service'Players'.LocalPlayer;
+    Player.Character.Humanoid:GetPropertyChangedSignal'WalkSpeed':Connect(function()
+    Player.Character.Humanoid.WalkSpeed = getgenv().WalkSpeedValue;
+    end)
+    Player.Character.Humanoid.WalkSpeed = getgenv().WalkSpeedValue;
+    end)
+local SectionTp = Main:NewSection("TP")
+SectionTp:NewTextBox("Players Username", "Enter Username not nickname", function(txt)
+	targetUsername = txt
+end)
+SectionTp:NewButton("TP TO PLAYER", ".", function()
+    players = game:GetService("Players")
+    targetPlayer = players:FindFirstChild(targetUsername)
+    players.LocalPlayer.Character:MoveTo(targetPlayer.Character.HumanoidRootPart.Position)
+end)
+local SectionAfk = Main:NewSection("ANTI-AFK")
+SectionAfk:NewButton("Launch Afk Script", ".", function()
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/RTrade/Voidz/main/AntiAFK.lua'),true))()
+end)
+local SectionRape = Main:NewSection("RAPE SCRIPT")
+SectionRape:NewButton("Rape Dat Boi", ".", function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/MontyEAG/scripts/main/rapa.lua'),true))()
+end)
